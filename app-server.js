da86af5e77e5517d18fa6e8558c686abae67af55
Christian <iancometa@gmail.com>
2016-06-29 00:31:41 +1000

    app-server.js

---
+++ app-server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static('./public'));
+app.use(express.static('./node_modules/bootstrap/dist/css'));
+app.use(express.static('./node_modules/react/dist'));
+app.use(express.static('./node_modules/react-dom/dist'));
+app.listen(3000);
+console.log('running at http://localhost:3000');
